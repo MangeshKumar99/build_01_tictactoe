7d70ec49ca20044b1b3191b19494d3f002244d25
Mangesh Kumar <mangesh.kumar@mintree.com>
2022-03-28 12:05:14 +0530

    modified css and added letter onClick

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,43 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'tictactoe';
+  ticTacToeArray:string[][]=[['','',''],['','',''],['','','']]
+  isOTurn:boolean=true;
+  onCellClick(cell:string){
+    console.log(`${cell} clicked !`);
+    switch(cell){
+      case '1':
+        this.isOTurn ? this.ticTacToeArray[0][0]='O' : this.ticTacToeArray[0][0]='X';
+        break;
+      case '2':
+        this.isOTurn ? this.ticTacToeArray[0][1]='O' : this.ticTacToeArray[0][1]='X';
+        break;
+      case '3':
+        this.isOTurn ? this.ticTacToeArray[0][2]='O' : this.ticTacToeArray[0][2]='X';
+        break;
+      case '4':
+        this.isOTurn ? this.ticTacToeArray[1][0]='O' : this.ticTacToeArray[1][0]='X';
+        break;
+      case '5':
+        this.isOTurn ? this.ticTacToeArray[1][1]='O' : this.ticTacToeArray[1][1]='X';
+        break;
+      case '6':
+        this.isOTurn ? this.ticTacToeArray[1][2]='O' : this.ticTacToeArray[1][2]='X';
+        break;
+      case '7':
+        this.isOTurn ? this.ticTacToeArray[2][0]='O' : this.ticTacToeArray[2][0]='X';
+        break;
+      case '8':
+        this.isOTurn ? this.ticTacToeArray[2][1]='O' : this.ticTacToeArray[2][1]='X';
+        break;
+      case '9':
+        this.isOTurn ? this.ticTacToeArray[2][2]='O' : this.ticTacToeArray[2][2]='X';
+        break;
+    }
+    this.isOTurn=!this.isOTurn;
+    console.log(this.ticTacToeArray);
+  }
+  ngOnInit(){
+    console.log(this.ticTacToeArray);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,8 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import {MatTooltipModule} from '@angular/material/tooltip';
+
 @NgModule({
   declarations: [
     AppComponent
@@ -12,7 +14,8 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatTooltipModule
   ],
   providers: [],
   bootstrap: [AppComponent]
